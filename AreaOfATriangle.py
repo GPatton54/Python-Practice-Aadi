bb0c3e3a8e4e79c63080748a00ac33575a04edda
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-25 14:06:17 -0400

    Add files via upload

---
+++ AreaOfATriangle.py
@@ -0,0 +1,6 @@
+base = int(input("What is the base of the triangle: "))
+height = int(input("What is the height of the triangle: "))
+
+firstval = base * height
+
+print("The area of the triangle is", firstval/2)
