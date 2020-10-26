7364db7f7bbbd64a847730287fc964abbbfcfafc
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-26 18:39:44 -0400

    Add files via upload

---
+++ PerimeterOfRect.py
@@ -0,0 +1,5 @@
+length = int(input("What is the length of your rectangle: "))*2
+
+width = int(input("What is the width of your rectangle: "))*2
+
+print("The perimeter of your rectangle is", length+width)
