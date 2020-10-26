1fefa32772f973f98910ad3aec0e116f9e921562
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-26 18:23:42 -0400

    Add files via upload

---
+++ FarmAnimalsProblem.py
@@ -0,0 +1,5 @@
+chickens = int(input("How many chickens: "))*2
+cows = int(input("How many cows: "))*4
+pigs = int(input("How many pigs: "))*4
+
+print("The total number of legs is", chickens+pigs+cows)
