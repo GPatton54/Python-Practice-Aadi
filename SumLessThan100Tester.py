1a1323bf7a1227c7b97282fa7c80a086ab1a505c
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-26 23:06:21 -0400

    Add files via upload

---
+++ SumLessThan100Tester.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+if a + b < 100:
+  print("The sum is less than 100")
+else:
+  print("The sum is greater than or equal to 100")
