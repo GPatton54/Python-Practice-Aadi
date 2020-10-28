e4c4a0e225866ad25b8088825842a9e22d04a198
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-28 17:19:31 -0400

    Add files via upload

---
+++ DoTheyDivideEvenly.py
@@ -0,0 +1,7 @@
+a = int(input("What is the first number: "))
+b = int(input("What is the second number: "))
+
+if a % b == 0:
+  print("These numbers divide evenly.")
+else: 
+  print("These numbers do not divide evenly.") 
