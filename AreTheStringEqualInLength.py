14e1ec328799b68bbc6b0b668eef9405ca379206
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-10-29 08:22:45 -0400

    Add files via upload

---
+++ AreTheStringEqualInLength.py
@@ -0,0 +1,7 @@
+str1=str(input("First string: "))
+str2=str(input("Second string: "))
+
+if len(str1)==len(str2):
+  print("The two are equal in character length. ")
+else:
+  print("The two are unequal in character length. ")
