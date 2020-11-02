e2f72f224bea495abb9280e660f4fe16ddad7de6
GPatton54 <71937164+GPatton54@users.noreply.github.com>
2020-11-02 15:48:51 -0500

    Add files via upload

---
+++ Take List As Input.py
@@ -0,0 +1,8 @@
+lst = []
+num = int(input("Enter the list size : "))
+
+for i in range(0, num):
+    print("Enter number at location", i, ":")
+    idk = int(input())
+    lst.append(idk)
+print(lst)
